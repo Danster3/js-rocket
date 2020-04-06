b47556d07186ec6a412177710046f615e01fa197
Danster3 <45056464+Danster3@users.noreply.github.com>
2020-04-07 11:52:14 +1200

    added css an js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var car = {
+    make: 'BMW',
+    type: 'Fast',
+    color: 'blue',
+    isTurnedOn: true,
+    numberOfWheels: 4,
+    seats: ['seat 1','seat 2','seat 3','seat 4'],
+    
+    trunOn: function (){
+        this.isTurnedOn = true;
+    },
+
+    turnOff: function(){
+        this.isTurnedOn = false;
+    },
+
+    fly: function(){
+        alert('fly');
+    }
+
+
+};
+
+
+console.log('hello there freiends'); 
